cf2c084a44e484f011981b46acf71e8a26cd0ca4
adi <kubatbekov888@gmail.com>
2019-06-28 19:18:50 +0600

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String [] name = {" Аскар", " Курман"," Алмаз"};
+        for (int i=0; i<name.length; i++)
+
+
+
+
+        switch (i) {
+            case 0:
+                System.out.println("Доброе утро" + name[0]);
+                break;
+            case 1:
+                System.out.println("Добрый день" + name[1]);
+                break;
+            case 2:
+                System.out.println("добрый вечер" + name[2]);
+                break;
+        }
+
+
+    }
+}
